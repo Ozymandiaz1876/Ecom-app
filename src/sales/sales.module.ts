4fe0b5f3ea566f16c8b117c2730ea6e81aba692c
Chandresh sharma <chandreshsharma06.cs@gmail.com>
2024-09-07 19:26:47 +0530

    [ECOM] : fix(sales import )  :fix coupons module import bug

--- src/sales/sales.module.ts
+++ src/sales/sales.module.ts
@@ -3,9 +3,10 @@ import { OrdersModule } from 'src/orders/orders.module';
 import { ProductsModule } from 'src/products/products.module';
 import { SalesController } from './sales.controller';
 import { SalesService } from './sales.service';
+import { CouponsModule } from 'src/coupons/coupons.module';
 
 @Module({
-  imports: [OrdersModule, ProductsModule],
+  imports: [OrdersModule, ProductsModule, CouponsModule],
   controllers: [SalesController],
   providers: [SalesService],
 })
